3d67e1b15f9121b7300489e4b3760fb8312f337d
menezesds <menezesds@hotmail.com>
2024-01-11 15:29:46 -0300

    Work with fs and create a getFile function in the index.js. Create a error handling function for the getFile.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('Hello world!'));
+function erroHandling(erro){
+    throw new Error(chalk.red(erro.code, 'Theres no file in the directory'));
+}
+
+function getFile(path){
+    const ecoding = 'utf-8';
+    fs.readFile(path, ecoding, (erro, text) => {
+        if(erro) erroHandling(erro);
+        console.log(chalk.green(text));
+    })
+}
+
+getFile('./src/text.md');
