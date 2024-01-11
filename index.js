1905a1f021d4f0ebd3fb0e10f133312ba08a7edf
menezesds <menezesds@hotmail.com>
2024-01-11 16:30:08 -0300

    Refactore of index.js file Using async await.

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ import fs from 'fs';
 function erroHandling(erro){
     throw new Error(chalk.red(erro.code, 'Theres no file in the directory'));
 }
-
-function getFile(path){
+async function getFile(path){
     const ecoding = 'utf-8';
-    fs.readFile(path, ecoding, (erro, text) => {
-        if(erro) erroHandling(erro);
-        console.log(chalk.green(text));
-    })
+    try{
+        const texto = await fs.promises.readFile(path, ecoding);
+        console.log(chalk.green(texto));
+    } catch(erro){erroHandling(erro)}
 }
 
 getFile('./src/text.md');
