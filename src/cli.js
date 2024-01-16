c1be4331772f68f4ed5a8724566afa12a312440d
menezesds <menezesds@hotmail.com>
2024-01-16 06:57:34 -0300

    Create the validation function

--- src/cli.js
+++ src/cli.js
@@ -2,18 +2,29 @@ import chalk from 'chalk';
 import fs from 'fs';
 
 import getExtracedFile from './index.js';
+import validateLinkList from './http-validation.js';
+
 
 const args = process.argv;
 
-function printList(list, path = ''){
-    console.log(
-        chalk.black.bgYellow(`Lita de links:`), 
-        chalk.black.bgGreen(path),
-        list);
+async function printList(validation, list, path = ''){
+    if(validation) {
+        console.log(
+            chalk.black.bgYellow(`Valid links:`), 
+            chalk.black.bgGreen(path),
+            await validateLinkList(list));
+    } else {
+        console.log(
+            chalk.black.bgYellow(`List of links:`), 
+            chalk.black.bgGreen(path),
+            list);
+
+    }
 }
 
 async function textProcess(args) {
     const path = args[2];
+    const validation = args[3] === '--validate' ? true : false;
 
     try {
         fs.lstatSync(path);
@@ -26,12 +37,12 @@ async function textProcess(args) {
 
     if(fs.lstatSync(path).isFile() === true) {
         const text = await getExtracedFile(path);
-        printList(text);
+        printList(validation, text);
     } else if(fs.lstatSync(path).isDirectory() === true) {
         const files = await fs.promises.readdir(path);
         files.forEach(async file => {
             const text = await getExtracedFile(`${path}/${file}`);
-            printList(text, path);
+            printList(validation, text, path);
         });
     }
 }
